2d3dc2b8d1131055c03fcc548aa971b0ea5c1160
“Ryan” <ryanlui1605@gmail.com>
2024-05-16 02:55:16 +0100

    updated

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,92 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "Arxiv2023": {
+        "author": "Hesham Sakr and others",
+        "journal": "ArXiv",
+        "title": "2307.12128v1",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "url": "https://arxiv.org",
+        "year": "2023"
+    },
+    "Chaudhary2020": {
+        "author": "Sarika Chaudhary and Pooja Nagpal and Yomesh Devgan",
+        "doi": "10.21276/ijircst.2020.8.3.2",
+        "journal": "International Journal of Innovative Research in Computer Science & Technology (IJIRCST)",
+        "number": "3",
+        "title": "AI Based Traffic and Automobile Monitoring System",
+        "type": "article",
+        "url": "https://doi.org/10.21276/ijircst.2020.8.3.2",
+        "volume": "8",
+        "year": "2020"
+    },
+    "Chaudhary2023": {
+        "author": "Sarika Chaudhary and others",
+        "doi": "Available in the file",
+        "journal": "Sensors",
+        "title": "Sensors Article",
+        "type": "article",
+        "url": "https://www.mdpi.com/journal/sensors",
+        "year": "2023"
+    },
+    "Farahdel2022": {
+        "author": "Arash Farahdel and Seyed Shahim Vedaei and Khan Wahid",
+        "booktitle": "14th IEEE International Conference on Computational Intelligence and Communication Networks (CICN)",
+        "doi": "10.1109/cicn.2022.51",
+        "title": "An IoT Based Traffic Management System Using Drone and AI",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1109/cicn.2022.51",
+        "year": "2022"
+    },
+    "Kishore2023": {
+        "author": "Bala Kishore M. and Srinivasa Reddy I. and L. Sujihelen and A. Sivasangari and A. Christy",
+        "booktitle": "7th International Conference on Intelligent Computing and Control Systems (ICICCS-2023)",
+        "doi": "10.1109/ICICCS56967.2023.10142605",
+        "title": "Artificial Intelligence (AI) Based Detection of Traffic Violations by Two-Wheeler Vehicles",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1109/ICICCS56967.2023.10142605",
+        "year": "2023"
+    },
+    "Sakr2023": {
+        "author": "Hesham Sakr",
+        "journal": "ResearchGate",
+        "title": "AI-Based Traffic System: A Novel Approach",
+        "type": "article",
+        "url": "https://www.researchgate.net/profile/Hesham-Sakr/publication/379065331_Al-based_Traffic_System_A_Novel_Approach/links/65fb41bda4857c7962655c77/Al-based-Traffic-System-A-Novel-Approach.pdf",
+        "year": "2023"
+    },
+    "Sakr2023a": {
+        "author": "Hesham Sakr and others",
+        "booktitle": "IEEE",
+        "doi": "10.1109/10053260",
+        "title": "Traffic Monitoring and Management System for Congestion Control using IoT and AI",
+        "type": "inproceedings",
+        "url": "https://ieeexplore.ieee.org/document/10053260",
+        "year": "2023"
+    },
+    "Sakr2023b": {
+        "author": "Hesham Sakr and others",
+        "booktitle": "IEEE",
+        "doi": "10.1109/9925777",
+        "title": "Hybrid AI-based Dynamic Re-routing Method for Dense Low-Altitude Air Traffic Operations",
+        "type": "inproceedings",
+        "url": "https://ieeexplore.ieee.org/abstract/document/9925777",
+        "year": "2023"
+    },
+    "Sakr2023c": {
+        "author": "Hesham Sakr and others",
+        "booktitle": "IEEE",
+        "doi": "10.1109/10205868",
+        "title": "Intelligent Traffic Management System: AI-Enabled IoT Traffic Lights to Mitigate Accidents and Minimize Environmental Pollution",
+        "type": "inproceedings",
+        "url": "https://ieeexplore.ieee.org/document/10205868",
+        "year": "2023"
+    },
+    "Uddin2021": {
+        "author": "Md. Imran Uddin and Md. Shahriar Alamgir and Md. Mahabubur Rahman and Muhammed Shahnewaz Bhuiyan and Md. Asif Moral",
+        "booktitle": "2021 2nd International Conference on Robotics, Electrical and Signal Processing Techniques (ICREST)",
+        "doi": "10.1109/ICREST51555.2021.9331256",
+        "title": "AI Traffic Control System Based on Deepstream and IoT Using NVIDIA Jetson Nano",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1109/ICREST51555.2021.9331256",
+        "year": "2021"
     }
 };
